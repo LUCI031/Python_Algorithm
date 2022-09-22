c51bdaa1adea3c06fda7202dac5d79d68ebfed87
LUCI031 <wlsrb031@naver.com>
2022-09-22 20:00:43 +0900

    220922 baekjoon implement solve 1

---
+++ baekjoon/Implement/220922-implement-12904.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+S = input().strip()
+T = input().strip()
+lst = []
+lst.append(T)
+while lst:
+    word = lst.pop()
+    if word == S:
+        print(1)
+        exit()
+    # case 1: add A
+    if len(word) < len(S):
+        continue
+    if word[-1] == 'A':
+        lst.append(word[:-1])
+    # case 2: reverse and B
+    if word[-1] == 'B':
+        r_word = word[:-1]
+        lst.append(r_word[::-1])
+print(0)
