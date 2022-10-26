ec56d4221bf835eef3a2b6ac736f3de6fcfcb60b
LUCI031 <wlsrb031@naver.com>
2022-10-26 19:45:33 +0900

    221026 baekjoon implement solve 1

---
+++ baekjoon/Implement/221026-implement-15662.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def check_right (start,d):
+    if start > n or gears[start-1][2] == gears[start][6]:
+        return
+    
+    if gears[start-1][2] != gears[start][6]:
+        check_right(start+1,-d)
+        gears[start].rotate(d)
+
+def check_left (start,d):
+    if start < 1 or gears[start][2] == gears[start+1][6]:
+        return
+    
+    if gears[start][2] != gears[start+1][6]:
+        check_left(start-1,-d)
+        gears[start].rotate(d)
+
+gears = {}
+
+n = int(input())
+for i in range(1,n+1):
+    gears[i] = deque(list(map(int, list(input().strip()))))
+
+k = int(input())
+for _ in range(k):
+    num, dirs = map(int,input().split()) 
+
+    check_right(num+1,-dirs)
+    check_left(num-1,-dirs)
+    gears[num].rotate(dirs)
+
+result = 0
+for i in range(n):
+    result += gears[i+1][0]
+
+print(result)
